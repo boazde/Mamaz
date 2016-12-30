0326c200b213caaf37383b3f6c5dd23a25dfbd5c
boaz <boazde@gmail.com>
2016-12-30 12:30:13 +0200

    payments add post request

--- Payments.js
+++ Payments.js
@@ -3,7 +3,7 @@ console.log(msg);
 
 
 var request = require('request');
-
+/*
 var options = {
     uri: 'https://api.icount.co.il/api/create_doc_auto.php',
     //method: 'POST',
@@ -16,4 +16,25 @@ request(options, function(error, response, body) {
     if (!error && response.statusCode == 200) {
         console.log(body) // Print the shortened url.
     }
+});
+
+*/
+
+var formData = {
+    my_field: 'my_value',
+    compID: '999',
+    user: 'admin',
+    pass: 'admin',
+    dateissued: '01.01.2017',
+    clientname: 'Yossi',
+    docType: 'invrec',
+    desc: 'This is test transaction ',
+    quantity: 1,
+    unitprice: 10
+};
+request.post({ url: 'https://api.icount.co.il/api/create_doc_auto.php', formData: formData }, function optionalCallback(err, httpResponse, body) {
+    if (err) {
+        return console.error('upload failed:', err);
+    }
+    console.log('Upload successful!  Server responded with:', body);
 });
